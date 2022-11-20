a5fd05cc5e61f77e6e20777ba4fc578f2cc5c450
Roman Dryndik <dryndikroman@gmail.com>
2022-11-20 13:15:41 +0100

    Add onsite course and staff table.

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -20,3 +22,24 @@ class OnlineCourse(Base):
 
     id = Column(Integer, primary_key=True)
     url = Column(String(50), nullable=False)
+
+
+class OnsiteCourse(Base):
+    __tablename__ = "onsite_courses"
+
+    id = Column(Integer, primary_key=True)
+    address = Column(String(50), nullable=False)
+    time = Column(String(10), nullable=False)
+    days = Column(Integer, nullable=False)
+
+
+class Staff(Base):
+    __tablename__ = "staff"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    pesel = Column(String(11), unique=True, nullable=False)
+    phone = Column(String(20))
+    address = Column(String(50))
+    enrollment_date = Column(DateTime, nullable=False, default=datetime.now)
